15bb8d6ac53e6d627e9d1548fb4946c582db852f
Vinayak <maheshwarivinayak90@gmail.com>
2024-10-15 00:32:00 +0530

    font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extend: {},
     fontFamily: {
       'roboto': ['Roboto', 'sans-serif'],
-      'algerian': ['cursive'],
+      'algerian': ['helvetica', 'sans-serif'],
       'alata': ['Alata', 'sans-serif'],
     },
     colors: {
